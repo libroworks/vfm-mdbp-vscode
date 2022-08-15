278eab95f19322887d17e6111ba1be9e5924f808
lwohtsu <ohtsu-y1@libroworks.co.jp>
2022-08-15 13:28:23 +0900

    目次の生成テスト

--- exampleFiles/vivliostyle.config.js
+++ exampleFiles/vivliostyle.config.js
@@ -1,10 +1,14 @@
 // @ts-check
 const vivliostyleConfig = {
   entry: [
+    // 目次の生成テスト
+    { rel: 'contents', theme: '_css/fullpower.css' },
     // mdを指定するとMDBPの独自仕様部分と画像類が外れる
     'formattest.html',
     'formattest_copy.html'
   ], 
+  toc: 'toc.html',
+  tocTitle: '目次',
   output: [
     './merged_output.pdf',
   ],
